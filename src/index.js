d3c4c08bd1efd57e6b18eac85a73bed4b60cd262
Jeff Boulay <BoulayJ@DNB.com>
2017-04-11 09:24:29 -0500

    added auto updating

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TipCalculator from './tip-calculator/';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <TipCalculator />, document.getElementById('root')
 );
--- src/tip-calculator/index.js
+++ src/tip-calculator/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FormInput from './common/form-input';
+import FormInput from '../common/form-input';
 import TipButton from './tip-button';
 import PerPersonTotal from './per-person-total'
 export default class TipCalculator extends React.Component {
@@ -7,22 +7,31 @@ export default class TipCalculator extends React.Component {
         super(props);
         this.state = {
             checkTotal: 500,
+            tipPercent:0,
             people: 1,
             personTotal: 0
         };
 
+        this.updateTip = this.updateTip.bind(this);
         this.updateBill = this.updateBill.bind(this);
         this.updatePeopleCount = this.updatePeopleCount.bind(this);
     }
 
-    calculateTip = (tipPercent) => {
-        this.setState((prevState) => ({personTotal: prevState.checkTotal * tipPercent / prevState.people}));
+    calculateTip = () => {
+        this.setState((prevState) => ({personTotal: prevState.checkTotal*prevState.tipPercent/prevState.people}));
 	};
+	updateTip(tip) {
+        console.log(tip);
+        this.setState({tipPercent: tip});
+        this.calculateTip()
+    }
     updateBill(event) {
         this.setState({checkTotal: event.target.value});
+        this.calculateTip();
     };
     updatePeopleCount(event) {
         this.setState({people: event.target.value});
+        this.calculateTip()
     }
 	render() {
 		return (
@@ -31,10 +40,10 @@ export default class TipCalculator extends React.Component {
 				<FormInput label="What's your bill?" onChange={this.updateBill} name="check-amount" type="text" />
                 <FormInput label="How many people?"  onChange={this.updatePeopleCount} name="party" type="number" />
 				<h2>Tip percentage</h2>
-				<TipButton onClickFunction={this.calculateTip} tipPercent="1.05"/>
-                <TipButton onClickFunction={this.calculateTip} tipPercent="1.10"/>
-                <TipButton onClickFunction={this.calculateTip} tipPercent="1.15"/>
-                <TipButton onClickFunction={this.calculateTip} tipPercent="1.20"/>
+				<TipButton onClickFunction={this.updateTip} tipPercent="1.05"/>
+                <TipButton onClickFunction={this.updateTip} tipPercent="1.10"/>
+                <TipButton onClickFunction={this.updateTip} tipPercent="1.15"/>
+                <TipButton onClickFunction={this.updateTip} tipPercent="1.20"/>
 				<PerPersonTotal total={this.state.personTotal}/>
 			</div>
 
